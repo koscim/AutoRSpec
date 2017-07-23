9a7de742f5680689c33bd12a7eee3c942ea4318a
koscim <koscim@gmail.com>
2017-07-23 00:46:23 -0400

    Add files via upload
    Installs the 'rspec' gem, runs bundle init to create a new Gemfile, adds the 'pry' and 'rspec' libraries to the Gemfile, runs bundle within the directory to update the Gemfile dependencies, runs 'rspec --init' to initialize 'rspec', adds '--color' to '.rspec', removes the unnecessary 'mkdir "spec"' line now that a 'spec' folder is automatically created by running 'rspec --init', adds 'require 'spec_helper'' to the "filename_spec.rb" file, and adds 'require_relative '../../lib/filename.rb'' to the "filename_spec.rb" file.

--- create_spec.sh
+++ create_spec.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+gem install rspec
+bundle init
+echo "gem 'pry'" >> "Gemfile"
+echo "gem 'rspec'" >> "Gemfile"
+bundle
+rspec --init
+echo "--color" >> ".rspec"
 echo "Specify File Name:"
 read filename
 echo "Spec Folders and Files Created!"
 mkdir "lib"
 touch "lib/$filename.rb"
-mkdir "spec"
+# mkdir "spec"
 mkdir "spec/lib"
 specname="_spec.rb"
 touch "spec/lib/$filename$specname"
+echo "require 'spec_helper'" >> "spec/lib/$filename$specname"
+echo "require_relative '../../lib/$filename.rb'" >> "spec/lib/$filename$specname"
